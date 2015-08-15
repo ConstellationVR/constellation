d3dd324d4900c0273db0ef6bc9696dfc580a0bc9
Liezl Puzon <puzon@stanford.edu>
2015-08-15 03:00:39 -0700

    The generator

---
+++ Assets/CubeStart.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeStart : MonoBehaviour {
+	public int speed = 10000;
+	// Use this for initialization
+	void Start () {
+		Rigidbody rb = GetComponent<Rigidbody> ();
+		rb.AddForce (this.transform.forward * speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Generator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generator : MonoBehaviour {
+	public GameObject cubePrefab;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space")) {
+			Instantiate(cubePrefab, this.transform.position, this.transform.rotation);
+		}
+	}
+}
