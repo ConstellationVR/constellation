74d1ce02b3cc7f93696a606d241dbcc62fd31824
Liezl Puzon <puzon@stanford.edu>
2015-08-15 20:55:06 -0700

    Add other scripts

---
+++ Assets/ToggleSuggest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ToggleSuggest : MonoBehaviour {
+	
+	//public Text suggestText;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("e")){
+			// todo: say -- "SENT TO EMAIL"
+			//if (this.ToString().Length > 0) {
+			//	this.GetComponent<RectTransform>().anchoredPosition  = new Vector3(30,50,0);
+			//}
+		}
+	}
+}
